94e7a75ea616799674ca5a6c061d0efde7f14e25
Shunta-Iwasaki <niwaka0219@gmail.com>
2023-10-20 09:37:42 +0900

    refactor_ページトップボタン作成_0001

--- js/script_common.js
+++ js/script_common.js
@@ -62,6 +62,27 @@ function getScrolled() {
     return ( window.pageYOffset !== undefined ) ? window.pageYOffset: document.documentElement.scrollTop;
 }
 
+// トップに戻るボタンの要素を取得
+let topPage = document.querySelectorAll(".page-top");
+            
+// ボタンの表示・非表示
+// スクロール時に動作
+window.addEventListener("scroll", function(){
+    // 条件と一致する時、フェードイン用/フェードアウト用クラスを付与
+    ( getScrolled() > 100 ) ? topPage[0].classList.add("is-fadein"): topPage[0].classList.remove( 'is-fadein' );
+});
+
+// トップに移動する関数
+function scrollToTop() {
+    window.scroll({ top: 0, behavior: "smooth" });
+};
+            
+// イベント登録
+// トップページ遷移ボタン押下時に動作
+topPage[0].addEventListener("click", function() {
+    scrollToTop();
+});
+
 // チャットbot
 // チャットbotの表示・非表示
 let chatbot = document.querySelectorAll(".chatbot");
