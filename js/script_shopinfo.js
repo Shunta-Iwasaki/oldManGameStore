033a67be080b7b097fc4eac8ad962cc2024b2c35
Shunta-Iwasaki <niwaka0219@gmail.com>
2023-11-11 14:52:48 +0900

    refactor_微調整_0002

--- js/script_shopinfo.js
+++ js/script_shopinfo.js
@@ -66,3 +66,30 @@ searchTab.addEventListener("keypress",(e) => {
         window.location.href = "list.html?title=" + "検索結果：" + e.target.value;
     }
 })
+
+// ページトップ遷移
+// スクロール量を取得する関数
+function getScrolled() {
+    return ( window.pageYOffset !== undefined ) ? window.pageYOffset: document.documentElement.scrollTop;
+}
+
+// トップに戻るボタンの要素を取得
+let topPage = document.querySelectorAll(".page-top");
+            
+// ボタンの表示・非表示
+// スクロール時に動作
+window.addEventListener("scroll", function(){
+    // 条件と一致する時、フェードイン用/フェードアウト用クラスを付与
+    ( getScrolled() > 100 ) ? topPage[0].classList.add("is-fadein"): topPage[0].classList.remove( 'is-fadein' );
+});
+
+// トップに移動する関数
+function scrollToTop() {
+    window.scroll({ top: 0, behavior: "smooth" });
+};
+            
+// イベント登録
+// トップページ遷移ボタン押下時に動作
+topPage[0].addEventListener("click", function() {
+    scrollToTop();
+});
